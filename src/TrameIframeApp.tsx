5665c21a9a49ddc95b66169c2984df60f318a7e5
Jules BOURDAIS <jules.bourdais@kitware.com>
2024-12-23 12:34:51 +0100

    feat(releaseà): test auto release

--- src/TrameIframeApp.tsx
+++ src/TrameIframeApp.tsx
@@ -14,7 +14,6 @@ const TrameIframeApp: React.FC<TrameIframeAppProps> =
     let iframe: HTMLElement | null = null;
 
     useEffect(() => {
-      // lookup the iframe element
       iframe = document.getElementById(iframeId);
 
       if (iframe == null) {
